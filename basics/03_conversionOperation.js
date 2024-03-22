2e5e2755469e87f4580e1f253ae5b37ff13483cc
Shouvik1997 <83128921+Shouvik1997@users.noreply.github.com>
2024-03-22 14:17:55 +0000

    conversion operations

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score1 = "33"
+let score2 = "33abc"
+let score3 = null
+let score4 = undefined
+let score5 = false
+let score6 = "Shouvik"
+
+//console.table([typeof score1, typeof score2, typeof score3, typeof score4, typeof score5, typeof score6]);
+
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+let valueInNumber6 = Number(score6)
+
+//console.table([valueInNumber1, valueInNumber2, valueInNumber3, valueInNumber4, valueInNumber5, valueInNumber6]);
+
+/*
+    "33" -> 33
+    "33abc" -> NaN
+    false -> 0 , true -> 1
+*/
+
+let isLoggedIn = "Shouvik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+/*
+    1 -> true , 0 -> false
+    "" -> false
+    "String" -> true
+ */
+
+let number = 45
+
+let stringNumber = String(number)
+
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+// 45 -> "45"
